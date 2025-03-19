b18f29051f27cd340f12169d3a614ad767fc0906
Alan Yeh <67916855+alanyehliyuan@users.noreply.github.com>
2025-03-19 17:48:21 +0800

    Fix Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-/// <reference types="node" />
+// /// <reference types="node" />
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.NODE_ENV === "production" ? "/alanyehliyuan.github.io/" : "/",
+// });
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/alanyehliyuan.github.io/" : "/",
+  base: "/alanyehliyuan.github.io/", // 這行要確認！
 });
